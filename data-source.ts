d1644dc0b4f9d61e2c7fbc0f2a1bb9fba753bc6a
camilono <orellanasolec@gmail.com>
2025-07-28 04:11:31 -0400

    fix: Corrige rutas en data-source para despliegue en producción

--- data-source.ts
+++ data-source.ts
@@ -1,14 +1,18 @@
-// data-source.ts
- import { DataSource } from 'typeorm';
- import { config } from 'dotenv';
- 
- config(); 
- 
- export default new DataSource({
-   type: 'postgres',
-   url: process.env.DATABASE_URL,
-   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
-   entities: [__dirname + '/**/*.entity{.ts,.js}'],
-   migrations: [__dirname + '/migrations/*{.ts,.js}'],
- });
- 
+
+ // data-source.ts
+import { DataSource } from 'typeorm';
+import { config } from 'dotenv';
+config();
+
+export default new DataSource({
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' 
+       ? { rejectUnauthorized: false } 
+       : false,
+  
+  // --- CAMBIA ESTAS LÍNEAS ---
+  entities: ["dist/**/*.entity.js"],
+  migrations: ["dist/migrations/*.js"], 
+  // --------------------------
+});
